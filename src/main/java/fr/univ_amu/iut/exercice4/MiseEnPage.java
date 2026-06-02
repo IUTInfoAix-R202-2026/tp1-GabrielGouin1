ea839db39538cfbed84302426a738c18f9b20839
Gabriel Gouin <gabriel.gouin@etu.univ-amu.fr>
2026-06-02 18:08:29 +0000

    Exo4 fini

--- src/main/java/fr/univ_amu/iut/exercice4/MiseEnPage.java
+++ src/main/java/fr/univ_amu/iut/exercice4/MiseEnPage.java
@@ -1,6 +1,16 @@
 package fr.univ_amu.iut.exercice4;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 /**
@@ -29,21 +39,31 @@ public class MiseEnPage extends Application {
 
   @Override
   public void start(Stage primaryStage) {
-    // TODO exercice 4 : construire la maquette ci-dessus.
-    //
-    // Stratégie conseillée :
-    //
-    // 1. Un BorderPane comme racine (setTop, setCenter, setBottom).
-    // 2. Top    : un MenuBar avec deux Menu "Fichier" et "Aide".
-    // 3. Center : un GridPane avec 2 lignes / 2 colonnes :
-    //                (0,0) Label "Nom :"     | (1,0) TextField
-    //                (0,1) Label "Email :"   | (1,1) TextField
-    // 4. Bottom : un HBox contenant deux boutons "Valider" et "Annuler",
-    //             avec un peu d'espacement et un padding.
-    //
-    // Donne un id CSS utile sur les composants si tu veux les retrouver
-    // facilement (les tests utilisent lookup sur les classes ".text-field"
-    // et ".button", pas besoin d'id particulier).
+    BorderPane root = new BorderPane();
+
+    MenuBar menuBar = new MenuBar();
+    menuBar.getMenus().addAll(new Menu("Fichier"), new Menu("Aide"));
+    root.setTop(menuBar);
+
+    GridPane grid = new GridPane();
+    grid.setHgap(10);
+    grid.setVgap(10);
+    grid.setPadding(new Insets(10));
+
+    grid.add(new Label("Nom :"), 0, 0);
+    grid.add(new TextField(), 1, 0);
+    grid.add(new Label("Email :"), 0, 1);
+    grid.add(new TextField(), 1, 1);
+    root.setCenter(grid);
+
+    HBox buttons = new HBox(10);
+    buttons.setPadding(new Insets(10));
+    buttons.getChildren().addAll(new Button("Valider"), new Button("Annuler"));
+    root.setBottom(buttons);
+
+    Scene scene = new Scene(root, 400, 300);
+    primaryStage.setScene(scene);
+    primaryStage.show();
   }
 
   public static void main(String[] args) {
